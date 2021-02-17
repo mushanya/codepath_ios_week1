bcd12169ac7cae27319252e05669e0734bd5bb9c
Anya <mushanya@sas.upenn.edu>
2021-02-17 09:13:24 -0500

    app without bonus features

---
+++ flixter/FlixCell.swift
@@ -0,0 +1,27 @@
+//
+//  FlixCell.swift
+//  flixter
+//
+//  Created by Anya Mushakevich on 2/17/21.
+//
+
+import UIKit
+
+class FlixCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
